a162e91a37c7cf8a89b52008599e4d13ea450883
konstantin <kostel1995@gmail.com>
2022-07-14 21:08:25 +0300

    task done

---
+++ lesson21/task4/index.js
@@ -0,0 +1,29 @@
+export const getTitle = () => {
+  const elements = document.querySelector('.title');
+  const text = elements.textContent;
+  return text;
+};
+
+// =============
+
+export const getDescription = () => {
+  const element = document.querySelector('.about');
+  const text = element.innerText;
+  return text;
+};
+
+// =============
+
+export const getPlans = () => {
+  const elem = document.querySelector('.plans');
+  const html = elem.innerHTML;
+  return html;
+};
+
+// =============
+
+export const getGoal = () => {
+  const element = document.querySelector('.plans');
+  const html = element.outerHTML;
+  return html;
+};
