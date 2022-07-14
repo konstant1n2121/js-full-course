241d26baee50a2a15708e097963cb040dca11af4
konstantin <kostel1995@gmail.com>
2022-07-14 20:50:13 +0300

    done

--- lesson21/task3/index.js
+++ lesson21/task3/index.js
@@ -1,13 +1,13 @@
 // input: elements
 // output: array
 
-const getItemsList = () => {
+export const getItemsList = () => {
   const elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
   return elementsList;
 };
 
-const getItemsArray = () => {
+export const getItemsArray = () => {
   const elemList = document.querySelectorAll('.tool');
   const elementsArray = Array.from(elemList);
   console.dir(elementsArray);
