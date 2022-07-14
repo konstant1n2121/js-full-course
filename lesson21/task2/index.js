bddbb16ef46e19c242836e5fef814e5d75142c7e
konstantin <kostel1995@gmail.com>
2022-07-14 17:33:32 +0300

    some fix

--- lesson21/task2/index.js
+++ lesson21/task2/index.js
@@ -4,12 +4,12 @@
 // 1. find element with queryselector
 // 2. return this element
 
-const getTitleElement = () => {
+export const getTitleElement = () => {
   const titleElem = document.querySelector('.title');
   return console.dir(titleElem);
 };
 
-const getInputElement = () => {
+export const getInputElement = () => {
   const inputElem = document.querySelector('text');
   return console.dir(inputElem);
 };
